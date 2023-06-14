e5ece13ef71e5d3304f740edd62a08824e40c431
JorisPutteneers <joris-mx@hotmail.com>
2023-06-14 23:49:27 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,209 @@
+var about = {
+    "eveline": [
+        {
+            "images": [
+                "./content/about/eveline/header.png",
+                "./content/about/eveline/b.png",
+                "./content/about/eveline/a.png",
+                "./content/about/eveline/c.jpg"
+            ]
+        },
+        {
+            "text": "./content/about/eveline/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/about/eveline/header.png"
+        }
+    ],
+    "frank": [
+        {
+            "images": [
+                "./content/about/frank/s.png",
+                "./content/about/frank/header.png"
+            ]
+        },
+        {
+            "text": "./content/about/frank/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/about/frank/header.png"
+        }
+    ]
+} 
+var articles = {
+    "article_drawing": [
+        {
+            "images": [
+                "./content/articles/article_drawing/DAA.png",
+                "./content/articles/article_drawing/b.png",
+                "./content/articles/article_drawing/a.png",
+                "./content/articles/article_drawing/sssss.png",
+                "./content/articles/article_drawing/xxx.png",
+                "./content/articles/article_drawing/IMG_9752-768x576.jpg",
+                "./content/articles/article_drawing/IMG_9711-768x576.jpg",
+                "./content/articles/article_drawing/cloud.jpg",
+                "./content/articles/article_drawing/matrix-1.jpg",
+                "./content/articles/article_drawing/cloud-2-1.jpg",
+                "./content/articles/article_drawing/movement-1.jpg",
+                "./content/articles/article_drawing/robinschaeverbekesmall-1-768x380.jpg",
+                "./content/articles/article_drawing/cropped-banner-miaw-1-768x401.jpg",
+                "./content/articles/article_drawing/Recorded-personal-movements_Q320.jpg",
+                "./content/articles/article_drawing/IMG_5627-reduced-768x1024.jpg",
+                "./content/articles/article_drawing/Thierry-Lagrange.jpg",
+                "./content/articles/article_drawing/header.jpg"
+            ]
+        },
+        {
+            "text": "./content/articles/article_drawing/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/articles/article_drawing/header.jpg"
+        }
+    ],
+    "drawing": [
+        {
+            "images": [
+                "./content/articles/drawing/DAA.png",
+                "./content/articles/drawing/c.png",
+                "./content/articles/drawing/b.png",
+                "./content/articles/drawing/a.png",
+                "./content/articles/drawing/sssss.png",
+                "./content/articles/drawing/IMG_9752-768x576.jpg",
+                "./content/articles/drawing/cloud.jpg",
+                "./content/articles/drawing/matrix-1.jpg",
+                "./content/articles/drawing/cloud-2-1.jpg",
+                "./content/articles/drawing/69-714x1024.jpg",
+                "./content/articles/drawing/movement-1.jpg",
+                "./content/articles/drawing/robinschaeverbekesmall-1-768x380.jpg",
+                "./content/articles/drawing/cropped-banner-miaw-1-768x401.jpg",
+                "./content/articles/drawing/Recorded-personal-movements_Q320.jpg",
+                "./content/articles/drawing/IMG_5627-reduced-768x1024.jpg",
+                "./content/articles/drawing/Thierry-Lagrange.jpg",
+                "./content/articles/drawing/header.jpg"
+            ]
+        },
+        {
+            "text": "./content/articles/drawing/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/articles/drawing/header.jpg"
+        }
+    ]
+} 
+var projects = {
+    "brick": [
+        {
+            "images": [
+                "./content/projects/brick/DAA.png",
+                "./content/projects/brick/c.png",
+                "./content/projects/brick/b.png",
+                "./content/projects/brick/a.png",
+                "./content/projects/brick/sssss.png",
+                "./content/projects/brick/IMG_9752-768x576.jpg",
+                "./content/projects/brick/IMG_9711-768x576.jpg",
+                "./content/projects/brick/cloud.jpg",
+                "./content/projects/brick/cloud-2-1.jpg",
+                "./content/projects/brick/69-714x1024.jpg",
+                "./content/projects/brick/movement-1.jpg",
+                "./content/projects/brick/robinschaeverbekesmall-1-768x380.jpg",
+                "./content/projects/brick/cropped-banner-miaw-1-768x401.jpg",
+                "./content/projects/brick/Recorded-personal-movements_Q320.jpg",
+                "./content/projects/brick/Thierry-Lagrange.jpg",
+                "./content/projects/brick/header.jpg"
+            ]
+        },
+        {
+            "text": "./content/projects/brick/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/projects/brick/header.jpg"
+        }
+    ],
+    "foundation": [
+        {
+            "images": [
+                "./content/projects/foundation/DAA.png",
+                "./content/projects/foundation/c.png",
+                "./content/projects/foundation/b.png",
+                "./content/projects/foundation/a.png",
+                "./content/projects/foundation/sssss.png",
+                "./content/projects/foundation/IMG_9752-768x576.jpg",
+                "./content/projects/foundation/IMG_9711-768x576.jpg",
+                "./content/projects/foundation/cloud.jpg",
+                "./content/projects/foundation/matrix-1.jpg",
+                "./content/projects/foundation/cloud-2-1.jpg",
+                "./content/projects/foundation/69-714x1024.jpg",
+                "./content/projects/foundation/movement-1.jpg",
+                "./content/projects/foundation/cropped-banner-miaw-1-768x401.jpg",
+                "./content/projects/foundation/Recorded-personal-movements_Q320.jpg",
+                "./content/projects/foundation/IMG_5627-reduced-768x1024.jpg",
+                "./content/projects/foundation/Thierry-Lagrange.jpg",
+                "./content/projects/foundation/header.jpg"
+            ]
+        },
+        {
+            "text": "./content/projects/foundation/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/projects/foundation/header.jpg"
+        }
+    ]
+} 
+var workshops = {
+    "ciry": [
+        {
+            "images": [
+                "./content/workshops/ciry/DAA.png",
+                "./content/workshops/ciry/c.png",
+                "./content/workshops/ciry/header.png",
+                "./content/workshops/ciry/b.png",
+                "./content/workshops/ciry/sssss.png",
+                "./content/workshops/ciry/IMG_9752-768x576.jpg",
+                "./content/workshops/ciry/IMG_9711-768x576.jpg",
+                "./content/workshops/ciry/matrix-1.jpg",
+                "./content/workshops/ciry/cloud-2-1.jpg",
+                "./content/workshops/ciry/69-714x1024.jpg",
+                "./content/workshops/ciry/movement-1.jpg",
+                "./content/workshops/ciry/robinschaeverbekesmall-1-768x380.jpg",
+                "./content/workshops/ciry/cropped-banner-miaw-1-768x401.jpg",
+                "./content/workshops/ciry/Recorded-personal-movements_Q320.jpg",
+                "./content/workshops/ciry/IMG_5627-reduced-768x1024.jpg",
+                "./content/workshops/ciry/Thierry-Lagrange.jpg"
+            ]
+        },
+        {
+            "text": "./content/workshops/ciry/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/workshops/ciry/header.png"
+        }
+    ],
+    "thoughts on sketching while walking the dog": [
+        {
+            "images": [
+                "./content/workshops/thoughts on sketching while walking the dog/DAA.png",
+                "./content/workshops/thoughts on sketching while walking the dog/c.png",
+                "./content/workshops/thoughts on sketching while walking the dog/b.png",
+                "./content/workshops/thoughts on sketching while walking the dog/a.png",
+                "./content/workshops/thoughts on sketching while walking the dog/sssss.png",
+                "./content/workshops/thoughts on sketching while walking the dog/IMG_9752-768x576.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/IMG_9711-768x576.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/matrix-1.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/cloud-2-1.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/69-714x1024.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/movement-1.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/robinschaeverbekesmall-1-768x380.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/cropped-banner-miaw-1-768x401.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/Recorded-personal-movements_Q320.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/IMG_5627-reduced-768x1024.jpg",
+                "./content/workshops/thoughts on sketching while walking the dog/Thierry-Lagrange.jpg"
+            ]
+        },
+        {
+            "text": "./content/workshops/thoughts on sketching while walking the dog/DESCRIPTION.txt"
+        },
+        {
+            "headerImg": "./content/workshops/thoughts on sketching while walking the dog/header.jpeg"
+        }
+    ]
+} 
